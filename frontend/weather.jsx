2ea711ce67f4331c9dc139155a6792ec743c8171
Logan Cooper <ldtcoop@gmail.com>
2017-09-09 16:13:24 -0700

    Add weather widget skeleton

---
+++ frontend/weather.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OPENWEATHER_API from 'apikey.js';
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Weather;
--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
+import Weather from './weather';
+
 
 class Root extends React.Component {
   render() {
     return (
       <div>
         <Clock />
+        <Weather />
       </div>
     );
   }
