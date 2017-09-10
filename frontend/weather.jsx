46723bfb2901cf720b49d1cf290e50880608288f
Logan Cooper <ldtcoop@gmail.com>
2017-09-09 18:38:58 -0700

    Fetch data from OpenWeather API

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -5,18 +5,59 @@ import OPENWEATHER_API from './apikey.js';
 class Weather extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {weather: null};
+    this.getWeather = this.getWeather.bind(this);
+    this.temp = this.temp.bind(this);
+    this.clearness = this.clearness.bind(this);
+    this.place = this.place.bind(this);
   }
 
   render() {
     return (
-      <div></div>
+      <div id="weather">
+        <h1>The weather in {this.place()}:</h1>
+        <h2>{this.temp()}()</h2>
+        <h2>{this.clearness()}</h2>
+      </div>
     );
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      console.log(pos);
-    });
+    navigator.geolocation.getCurrentPosition(this.getWeather);
+  }
+
+  getWeather(location) {
+    const lat = location.coords.latitude;
+    const lon = location.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&forecast?id=524901&APPID=${OPENWEATHER_API}`;
+    let weatherData = null;
+
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = () => {
+      if (request.status === 200 && request.readyState === XMLHttpRequest.DONE ) {
+        const data = JSON.parse(request.responseText);
+        this.setState( {weather: data} );
+      }
+    };
+
+    request.open('GET', url, true);
+    request.send();
+  }
+
+  temp() {
+    let kTemp = this.state.weather.main.temp;
+    return (kTemp * (9/5) - 459.67);
+  }
+
+  clearness() {
+    let clear = this.state.weather[0].main;
+    return clear;
+  }
+
+  place() {
+    let place = this.state.weather.name;
+    return place;
   }
 }
 
